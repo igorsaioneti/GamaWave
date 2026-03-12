c81a48cdd01f104d43bf1edc64345fd851f729ea
igorsaioneti <igorsaioneti@gmail.com>
2026-03-11 21:50:22 -0300

    Add files via upload

---
+++ Tela1/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Tela1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            button1 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(225, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 14);
+            label1.TabIndex = 1;
+            label1.Text = "Admin Barbearia";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Captura_de_tela_2026_03_11_2105341;
+            pictureBox1.Location = new Point(136, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(83, 80);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Franklin Gothic Book", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(239, 26);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Administrator";
+            // 
+            // button1
+            // 
+            button1.Image = Properties.Resources.Captura_de_tela_2026_03_11_211908;
+            button1.Location = new Point(225, 44);
+            button1.Name = "button1";
+            button1.Size = new Size(85, 23);
+            button1.TabIndex = 5;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(139, 108);
+            label3.Name = "label3";
+            label3.Size = new Size(96, 16);
+            label3.TabIndex = 6;
+            label3.Text = "Nome Completo";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(298, 108);
+            label4.Name = "label4";
+            label4.Size = new Size(39, 16);
+            label4.TabIndex = 7;
+            label4.Text = "E-mail";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Century Gothic", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(139, 155);
+            label5.Name = "label5";
+            label5.Size = new Size(52, 16);
+            label5.TabIndex = 8;
+            label5.Text = "Telefone";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Century Gothic", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(297, 155);
+            label6.Name = "label6";
+            label6.Size = new Size(59, 16);
+            label6.TabIndex = 9;
+            label6.Text = "Endereço";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(139, 127);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(138, 20);
+            textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(298, 127);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(190, 20);
+            textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(139, 174);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(138, 20);
+            textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(298, 174);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(190, 20);
+            textBox4.TabIndex = 13;
+            // 
+            // button2
+            // 
+            button2.Image = Properties.Resources.Captura_de_tela_2026_03_11_212952;
+            button2.Location = new Point(381, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(34, 23);
+            button2.TabIndex = 15;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Image = Properties.Resources.Captura_de_tela_2026_03_11_213456;
+            button3.Location = new Point(2, 76);
+            button3.Name = "button3";
+            button3.Size = new Size(118, 23);
+            button3.TabIndex = 16;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Image = Properties.Resources.Captura_de_tela_2026_03_11_213606;
+            button4.Location = new Point(2, 134);
+            button4.Name = "button4";
+            button4.Size = new Size(118, 22);
+            button4.TabIndex = 17;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Image = Properties.Resources.Captura_de_tela_2026_03_11_213656;
+            button5.Location = new Point(2, 18);
+            button5.Name = "button5";
+            button5.Size = new Size(118, 23);
+            button5.TabIndex = 18;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Image = Properties.Resources.Captura_de_tela_2026_03_11_213819;
+            button6.Location = new Point(2, 105);
+            button6.Name = "button6";
+            button6.Size = new Size(118, 23);
+            button6.TabIndex = 19;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Image = Properties.Resources.Captura_de_tela_2026_03_11_213910;
+            button7.Location = new Point(2, 47);
+            button7.Name = "button7";
+            button7.Size = new Size(118, 23);
+            button7.TabIndex = 20;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Image = Properties.Resources.Captura_de_tela_2026_03_11_214125;
+            button8.Location = new Point(2, 162);
+            button8.Name = "button8";
+            button8.Size = new Size(119, 32);
+            button8.TabIndex = 21;
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(6F, 14F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.WhiteSmoke;
+            ClientSize = new Size(527, 234);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Font = new Font("Tw Cen MT Condensed Extra Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Button button1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+    }
+}
